2fb80db4e7fcee5c619e7de7b2a04c44374cd2ac
charlie <charliephung@gmail.com>
2018-07-25 18:22:17 +0300

    fix bugs

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,11 @@ import api from "./api/api";
 import { SET_USER_TOKEN, REMOVE_USER_TOKEN } from "../constants/actionTypes";
 
 export const actGoogleLogin = data => dispatch => {
+  console.log(data);
+
   api.auth.withGoogle(data).then(res => {
+    console.log(res);
+
     dispatch({
       type: SET_USER_TOKEN,
       payload: res
